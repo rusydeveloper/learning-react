a9b464279925c0c65b89e543a06b656e25edab70
rusydeveloper <rusy.developer@gmail.com>
2020-02-29 21:37:44 +0700

    add production

---
+++ build/precache-manifest.deff513c586ced01b26fc071d786d7f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff59dda4f16b86b374d35d18d2dab8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8df7a67eff9934adb4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c5a9ce1fda8ba6742ed",
+    "url": "/static/js/2.2c4afd78.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2c4afd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8df7a67eff9934adb4",
+    "url": "/static/js/main.e6854a49.chunk.js"
+  },
+  {
+    "revision": "c4a7f7998775d9b7351d",
+    "url": "/static/js/runtime-main.a4bd9d66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
