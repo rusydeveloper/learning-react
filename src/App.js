928fa377b4030d2f035118b59237a07db2fc8f1d
rusydeveloper <rusy.developer@gmail.com>
2020-03-31 07:25:28 +0700

    edited Cart location

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Monitoring from "./pages/Monitoring";
 import Login from "./pages/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Cart from "./components/Cart";
 import { Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
