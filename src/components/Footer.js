ef31e75a4f86aa7076b76bdd6d018ffb22ddd45f
rusydeveloper <rusy.developer@gmail.com>
2020-05-13 21:54:54 +0700

    added suppliers logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import logoFKBK from "../assets/logo-FKBK.png";
+import logoBulog from "../assets/logo-BULOG.png";
+import logoDT from "../assets/logo-DT.png";
+import logoITB from "../assets/logo-ITB.png";
+import logoNectico from "../assets/logo-nectico.png";
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.url_api = "https://fkbk-api.nectico.com";
-    this.state = {};
-  }
-
-  render() {
-    return <div className="footer"></div>;
-  }
+function Footer() {
+  return (
+    <div>
+      <small className="supported-by-text">Supported By</small>
+      <div className="flex-container-brand footer-container">
+        <div>
+          <img src={logoFKBK} className="logo-footer-fkbk" alt="logo" />
+        </div>
+        <div>
+          <img src={logoBulog} className="logo-footer-fkbk" alt="logo" />
+        </div>
+        <div>
+          <img src={logoDT} className="logo-footer-dt" alt="logo" />
+        </div>
+        <div>
+          <img src={logoITB} className="logo-footer-itb" alt="logo" />
+        </div>
+        <div>
+          <img src={logoNectico} className="logo-footer-nectico" alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Footer;
